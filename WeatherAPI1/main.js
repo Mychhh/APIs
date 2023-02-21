9f66ef39d729418dd0b2e6d224f2cd6dd1b6fb63
Michael <michaeldrpabaya@gmail.com>
2023-02-21 17:12:56 +0800

    Added Geolocation

--- WeatherAPI1/main.js
+++ WeatherAPI1/main.js
@@ -74,7 +74,6 @@ document.querySelector('.search-bar').addEventListener('keypress', function (eve
 })
 
 const weatherBackground = (temperature) => {
-
     let backGroundImage = ''
 
     if (temperature < 0) {
@@ -108,4 +107,70 @@ const weatherBackground = (temperature) => {
     document.body.style.backgroundImage = backGroundImage
 }
 
-weather.fetchWeather('Bulacan')
+//geolocation object
+let geocode = {
+    reverseGeocode: (latitude, longitude) => {
+        var api_key = '2e86df76549f45f28cea2729ad70a012';
+        // var latitude = '51.0';
+        // var longitude = '7.0';
+
+        var api_url = 'https://api.opencagedata.com/geocode/v1/json'
+
+        var request_url = api_url
+            + '?'
+            + 'key=' + api_key
+            + '&q=' + encodeURIComponent(latitude + ',' + longitude)
+            + '&pretty=1'
+            + '&no_annotations=1';
+
+        // see full list of required and optional parameters:
+        // https://opencagedata.com/api#forward
+
+        // http request
+        var request = new XMLHttpRequest();
+        request.open('GET', request_url, true);
+
+        request.onload = function () {
+            // see full list of possible response codes:
+            // https://opencagedata.com/api#codes
+
+            if (request.status === 200) {
+                // Success!
+                var data = JSON.parse(request.responseText);
+                //alert(data.results[0].formatted); // print the location
+                // console.log(data.results[0])
+                // gets the current city location
+                weather.fetchWeather(data.results[0].components.city)
+            } else if (request.status <= 500) {
+                // We reached our target server, but it returned an error
+
+                console.log("unable to geocode! Response code: " + request.status);
+                var data = JSON.parse(request.responseText);
+                console.log('error msg: ' + data.status.message);
+            } else {
+                console.log("server error");
+            }
+        };
+
+        request.onerror = function () {
+            // There was a connection error of some sort
+            console.log("unable to connect to server");
+        };
+
+        request.send();  // make the request
+    },
+    getLocation : function () {
+        // gets the data from success return value in getCurrentPosition
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(success, console.error)
+        }else{
+            weather.fetchWeather('Philippines')
+        }
+        
+        function success (data){
+            geocode.reverseGeocode(data.coords.latitude, data.coords.longitude)
+        }
+    }
+}
+
+geocode.getLocation()
--- WeatherAPI1/notes.md
+++ WeatherAPI1/notes.md
@@ -1,3 +1,8 @@
-Docs: https://openweathermap.org/api
+Weather API Docs: https://openweathermap.org/api
 Current Weather Data: https://openweathermap.org/current
 Image Generator: https://deepai.org/machine-learning-model/fantasy-world-generator
+
+
+Geolocation Docs: https://opencagedata.com/dashboard#geocoding
+Geolocation Request: https://opencagedata.com/dashboard#geocoding
+
